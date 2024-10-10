7b1f3d6532cc52547d87bc5f219ca5ee4ee02afc
Leonardo~ <140676545+leonardo46843@users.noreply.github.com>
2024-10-10 10:16:57 +0700

    preparation & prerequisite

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Kanit"],
+      },
+      colors: {
+        primary: "#f59e0b",
+        secondary: "#1e40af",
+      },
+    },
+  },
+  plugins: [],
+};
